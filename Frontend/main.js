ca60e99c665adccc4cad5bfeb2b4f6bae373ad69
Amirakc2061@gmail.com <abhishekbhatta110@gmail.com>
2025-02-17 02:38:43 +0545

    file added

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
-  $(".text").textillate({
-    loop : true,
-    sync : true,
-    in : {
-      effect : "bounceIn",
-    },
-    out : {
-      effect : "bounceOut",
-    }
-  })
+  if ($(".newText").length > 0) {
+      console.log("✅ Element found! Running Textillate...");
+      $(".newText").lettering();
+      $(".newText").textillate({
+          loop: true,
+          sync: true,
+          in: { effect: "bounceIn" },
+          out: { effect: "bounceOut" },
+      });
+  } else {
+      console.log("❌ Element NOT found! Check your HTML.");
+  }
 });
---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+import eel
+  
+
+eel.init('frontend') # To point this library we write this
+
+# To open app mode for our project any browser provide app mode
+os.system('start msedge.exe --app="http://localhost:8000/index.html" ');
+
+#   To tell which one is frontend file 
+
+eel.start('index.html', mode=None, host='localhost',block=True)
